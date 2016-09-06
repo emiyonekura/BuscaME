7d0083ed9cf14129ead76caa4bd0b33a879358e3
Emiko <emiko.yonekura@gmail.com>
2016-09-06 20:11:15 -0300

    modelos pp

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,4 @@
 class Product < ApplicationRecord
   belongs_to :business
+  belongs_to :category
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160906230403) do
+ActiveRecord::Schema.define(version: 20160906230919) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,12 @@ ActiveRecord::Schema.define(version: 20160906230403) do
     t.datetime "updated_at",  null: false
   end
 
+  create_table "categories", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "products", force: :cascade do |t|
     t.string   "name"
     t.string   "description"
@@ -31,8 +37,11 @@ ActiveRecord::Schema.define(version: 20160906230403) do
     t.integer  "business_id"
     t.datetime "created_at",                  null: false
     t.datetime "updated_at",                  null: false
+    t.integer  "category_id"
     t.index ["business_id"], name: "index_products_on_business_id", using: :btree
+    t.index ["category_id"], name: "index_products_on_category_id", using: :btree
   end
 
   add_foreign_key "products", "businesses"
+  add_foreign_key "products", "categories"
 end
